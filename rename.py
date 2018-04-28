bc3cd15f0c9e812fad9bd655cf34efd29367feb7
Remi-Boutin <boutin.remi95@gmail.com>
2018-04-28 10:52:41 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -12,7 +12,9 @@ import time
 import yaml
 
 
+
 t1= time.time()
+dossier=[path]
 #sous dossiers dans lesquels sont stockes tous les comptes
 list_names=['CNEWS','Europe1','FN_Officiel','Le_Figaro','lemonde','lesRepublicains','partisocialiste','Challenges','EnMarchefr','FranceInsoumise','LesEchos',
 'MarianneleMag','RTLFrance']
@@ -20,7 +22,7 @@ real_name=list()
 
 #on met tous les comptes les uns a la suite des autres dans la list real_name
 for k in list_names:
-    os.chdir('C:/Users/remib/Documents/ENSAE/2A/Statapp/graph/twitter-users/'+k)
+    os.chdir(dossier+'twitter-users/'+k)
     f = glob.glob('*.json')
     real_name.extend(map(lambda i: i[0:-5], f))
 
@@ -29,7 +31,7 @@ rn_array=np.unique(rn_array)
 
 print rn_array.size
 print len(rn_array)
-os.chdir('C:/Users/remib/Documents/ENSAE/2A/Statapp/graph/')
+os.chdir(dossier)
 
 #dictionnaire qui d'un numero renvoie l'id de depart
 dico={i:rn_array[i] for i in range(0,len(rn_array))}
@@ -44,25 +46,25 @@ with open("idtonum.json",'w') as outf:
 
 #on renomme les fichiers avec leur nouveau nom
 for  h in list_names:
-    os.chdir('C:/Users/remib/Documents/ENSAE/2A/Statapp/graph/twitter-users/'+h)
+    os.chdir(dossier+'twitter-users/'+h)
     f = glob.glob('*.json')
     real_name = map(lambda i: i[0:-5], f)
     for k in f:
         d=dico[k[0:-5]]
         os.rename(k, str(d)+'.json' )
 
-with open('C:/Users/remib/Documents/ENSAE/2A/Statapp/graph/idtonum.json') as outf:
+with open(dossier+'idtonum.json') as outf:
     dico=yaml.load(json.dumps(json.load(outf)))
     outf.close()
 
-with open('C:/Users/remib/Documents/ENSAE/2A/Statapp/graph/compte_id.json') as outf:
+with open(dossier+'compte_id.json') as outf:
     compte=yaml.load(json.dumps(json.load(outf)))
     outf.close()
 
 for k in range(1,15):
     compte[str(k)]=dico[str(compte[str(k)])]
 
-with open('C:/Users/remib/Documents/ENSAE/2A/Statapp/graph/compte_id_edit.json','w') as outf:
+with open(dossier+'compte_id_edit.json','w') as outf:
     json.dump(compte,outf)
     outf.close()
 print time.time() - t1
